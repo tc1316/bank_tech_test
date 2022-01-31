d1af9cce16e6971c3aac7e5441dc21ec3851c92c
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2022-01-31 17:17:55 +0000

    Updated withdraw/deposit methods to use pence as units;refactored to use helper method to create entries in account statement

--- account.py
+++ account.py
@@ -1,35 +1,59 @@
-# Account class
-from dataclasses import dataclass
+from datetime import date
+from dataclasses import dataclass, field
+from typing import List
 
 
-@dataclass()
+@dataclass(order=True)
 class Account:
     forename: str
     surname: str
-    date_created: str
-    balance: int = 0
+    balance: float = 0  # Stored in pence
+    date = date.today().strftime('%d/%m/%Y')
+    statement: List = field(default_factory=list)
 
-    def fullname(self):
-      return f"{self.forename} {self.surname}"
+    def __str__(self):
+        return f"{self.forename} {self.surname} - Outstanding balance: {self.balance / 100} - Date: {self.date}"
 
-    def credit(self, amount):
+    def entry_generator(self, amount, entry_type=None):
+        if entry_type == "deposit":
+            self.statement.append(
+                f"{self.date} || {(amount / 100):.2f} || || {(self.balance / 100):.2f}")
+        elif entry_type == "withdraw":
+            self.statement.append(
+                f"{self.date} ||  || {(amount / 100):.2f}|| {(self.balance / 100):.2f}")
+        else:
+            raise TypeError("Needs to be a deposit or withdrawal")
+
+    def deposit(self, amount):
+        amount *= 100
         self.balance += amount
-        return self.balance        
+        self.entry_generator(amount, entry_type="deposit")
+        return self.balance
 
-    def debit(self, amount):
+    def withdraw(self, amount):
         if self.balance - amount >= 0:
+            amount *= 100
             self.balance -= amount
+            self.entry_generator(amount, entry_type="withdraw")
             return self.balance
         else:
             raise ValueError("Insufficient funds")
 
+    def print_statement(self):
+        print('date || credit || debit || balance')
+        for i in range(len(self.statement) - 1, -1, -1):
+            print(self.statement[i])
+
 
 def main():
-    test_account = Account("Jeff", "Garlan", '10/03/2023')
+    test_account = Account("Jeff", "Garlan")
     print(test_account)
-    test_account.credit(100)
+    test_account.deposit(100)
+    test_account.deposit(50)
+    test_account.withdraw(25)
+    test_account.withdraw(10)
+    test_account.print_statement()
     print(test_account)
-    test_account.debit(201)
 
 
 if __name__ == '__main__':
--- test_account.py
+++ test_account.py
@@ -1,34 +1,41 @@
 import unittest
 from account import Account
+from datetime import date
+
 
 class TestCalc(unittest.TestCase):
-  def setUp(self):
-    self.client = Account(forename='Sue', surname='Smith', date_created='10/01/2023')
-    self.client2 = Account(forename='Jeff', surname='Garlan', date_created='10/01/2023', balance=100)
+    def setUp(self):
+        self.client = Account(
+            forename='Sue',
+            surname='Smith')
+        self.client2 = Account(
+            forename='Jeff',
+            surname='Garlan',
+            balance=10000)
 
-  def test_account_name(self):
-    self.assertEqual(self.client.forename, 'Sue')
-    self.assertEqual(self.client.surname, 'Smith')
-    self.assertEqual(self.client.fullname(), 'Sue Smith')
+    def test_account_name(self):
+        self.assertEqual(self.client.forename, 'Sue')
+        self.assertEqual(self.client.surname, 'Smith')
 
-  def test_account_date_creation(self):
-    self.assertEqual(self.client.date_created, '10/01/2023')
+    def test_date_defaults_to_today(self):
+        self.assertEqual(self.client.date, date.today().strftime('%d/%m/%Y'))
 
-  def test_initial_balance(self):
-    self.assertEqual(self.client.balance, 0)
-    self.assertEqual(self.client2.balance, 100)
+    def test_setting_initial_balance(self):
+        self.assertEqual(self.client.balance, 0)
+        self.assertEqual(self.client2.balance, 10000)
 
-  def test_credit_account(self):
-    self.assertEqual(self.client.credit(100), 100)
-    self.assertEqual(self.client2.credit(100),200)
-  
-  def test_debit_account(self):
-    with self.assertRaises(ValueError):
-      self.client.debit(101)
-    self.assertEqual(self.client2.debit(100),0)
+    def test_deposit(self):
+        self.assertEqual(self.client.deposit(100.00), 10000)
+        self.assertEqual(self.client.deposit(50), 15000)
+        self.assertEqual(self.client2.deposit(100.00), 20000)
+
+    def test_withdraw(self):
+        with self.assertRaises(ValueError):
+            self.client.withdraw(1)
+        self.assertEqual(self.client2.withdraw(100.00), 0)
+        with self.assertRaises(ValueError):
+            self.client2.withdraw(1)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-  
-
