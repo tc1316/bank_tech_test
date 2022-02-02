a1e682ae7f02dffbe335eae692d77e3dda886db6
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2022-02-02 11:30:03 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from account import Account, StatementPrinter
+
+def main():
+    test_account = Account("Jeff", "Garlin")
+    printer = StatementPrinter()
+    
+    test_account.set_date("10/01/2023")
+    test_account.deposit(1000)
+
+    test_account.set_date("13/01/2023")
+    test_account.deposit(2000)
+
+    test_account.set_date("14/01/2023")
+    test_account.withdraw(500)
+
+    printer.print_statement(test_account)
+
+if __name__ == '__main__':
+    main()
